20fbe247594b1328d46210cda80b08ed89cecb76
tuancntt <letrananhtien752007@gmail.com>
2024-12-24 11:17:44 +0700

    Add project files.

---
+++ bai2_Cong2SoNguyen.cpp
@@ -0,0 +1,24 @@
+// bai2_Cong2SoNguyen.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Cong2SoNguyen!\n";
+    /*
+    int so1 = 454;
+    int so2 = 6;
+    int tong = so1 + so2;
+    cout << "Tong 2 so tren la:" << tong;
+    */
+    double so1, so2;
+
+    cout << "Nhap so 1:";
+    cin >> so1;
+    cout << "Nhap so 2:";
+    cin >> so2;
+
+    double Tong = so1 + so2;
+    cout << "Tong 2 so tren la:" << Tong;
+}
